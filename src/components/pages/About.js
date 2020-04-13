f5fac0b6c5459f0dfdc6f4f623c4ce135e9a982a
Andrew <andrew.rutherford.6@gmail.com>
2020-04-13 11:02:09 +0700

    modified mobile styles and cleared console warnings

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const About = props => {
+const About = () => {
   return (
     <div className='container'>
       <h1 className='text-primary'>About This App</h1>
       <p className='my-1'>
         This app will generate a word cloud based off a specific subreddit.
-      </p>
-      <p>
-        <strong>Version: </strong>1.0.0
       </p>
     </div>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ReactWordcloud from 'react-wordcloud';
 import axios from 'axios';
 import { createWordCloud } from '../../utils/createWordCloud';
@@ -8,27 +8,23 @@ const Home = ({ reddit }) => {
   const [wordCloud, setWordCloud] = useState(null);
   const [subreddit, setSubreddit] = useState('');
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
 
-  const onSubmit = async e => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
     try {
       const res = await axios.get(`${reddit}${subreddit}.json`);
-      //const res = await axios.get(`${reddit}news.json`);
       const resData = res.data.data.children;
       setError(null);
-      const titles = resData.map(post => post.data.title);
+      const titles = resData.map((post) => post.data.title);
       const cloud = createWordCloud(titles);
       setWordCloud(cloud);
     } catch (err) {
       console.error(err);
       setError('No data found.  Try searching for another subreddit');
     }
-    setLoading(false);
   };
 
-  const onChange = e => {
+  const onChange = (e) => {
     setSubreddit(e.target.value);
   };
 
@@ -38,7 +34,7 @@ const Home = ({ reddit }) => {
         <h2 className='text-primary'>
           Search for a subreddit and visualize the most common words
         </h2>
-        <form onSubmit={onSubmit}>
+        <form className='grid-2-form' onSubmit={onSubmit}>
           <label>
             <input
               type='text'
@@ -47,9 +43,9 @@ const Home = ({ reddit }) => {
               onChange={onChange}
             />
           </label>
-          {error && <p>{error}</p>}
           <input type='submit' value='Submit' className='btn btn-primary' />
         </form>
+        {error && <p>{error}</p>}
       </div>
       <div className='container'>
         {wordCloud !== null && (
@@ -61,7 +57,7 @@ const Home = ({ reddit }) => {
 };
 
 Home.defaultProps = {
-  reddit: 'https://www.reddit.com/r/'
+  reddit: 'https://www.reddit.com/r/',
 };
 
 export default Home;
